310d13d4047c527964fe8542ba35337539fe777e
Allyson Robert <allyson.robert@aalto.fi>
2026-08-28 17:01:55 +0300

    Image scaling - multiplier and scaling decoupled

--- src/image_ops/scale_image.py
+++ src/image_ops/scale_image.py
@@ -1,4 +1,3 @@
-import settings
 import cv2 as cv
 from src.image.photograph import Photograph
 
@@ -8,13 +7,8 @@ def scale_image(photo: Photograph , multiplier: float = None):
     """
 
     # Copy the image so that the original is not edited
-    photo_copy = photo.copy()
-
-    if multiplier is None:
-       multiplier = min(settings.max_window[0] / photo_copy[0].shape[1],
-                        settings.max_window[1] / photo_copy[0].shape[0])
-
-    scaled_contents = cv.resize(photo_copy.get_image(), None, fx=multiplier, fy=multiplier, interpolation=cv.INTER_NEAREST)
-    scaled_photo = Photograph.from_image_data(scaled_contents, photo_copy.get_metadata())
+    photo_c = photo.copy()
+    scaled_contents = cv.resize(photo_c.get_image(), None, fx=multiplier, fy=multiplier, interpolation=cv.INTER_NEAREST)
+    scaled_photo = Photograph.from_image_data(scaled_contents, photo_c.get_metadata())
 
     return scaled_photo
