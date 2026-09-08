db0569c5dd8daea52543708d818fb2bd1c8a3a85
Allyson Robert <allyson.robert@aalto.fi>
2026-09-08 15:42:25 +0300

    Renamed photo to image

--- src/image_ops/scale_image.py
+++ src/image_ops/scale_image.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 from src.image.photograph import Photograph
 
-def scale_image(photo: Photograph , multiplier: float = None):
+def scale_image(image: Photograph, multiplier: float = 1.0):
     """
         Returns a scaled copy of the input Photograph
     """
 
     # Copy the image so that the original is not edited
-    photo_c = photo.copy()
-    scaled_contents = cv.resize(photo_c.get_image(), None, fx=multiplier, fy=multiplier, interpolation=cv.INTER_NEAREST)
-    scaled_photo = Photograph.from_image_data(scaled_contents, photo_c.get_metadata())
+    img = image.get_image().copy()
+    scaled_contents = cv.resize(img, None, fx=multiplier, fy=multiplier, interpolation=cv.INTER_NEAREST)
+    scaled_photo = Photograph(scaled_contents, img.get_metadata())
 
     return scaled_photo
