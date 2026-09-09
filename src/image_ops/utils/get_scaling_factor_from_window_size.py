316d567afdde67105d5dd14ed0078cb4b5d54a36
Allyson Robert <allyson.robert@aalto.fi>
2026-09-09 17:29:53 +0300

    bugfix: was incompatible with Photograph clasds

--- src/image_ops/utils/get_scaling_factor_from_window_size.py
+++ src/image_ops/utils/get_scaling_factor_from_window_size.py
@@ -6,6 +6,8 @@ def get_scaling_factor_from_window_size(photo: Photograph):
     """
         Returns the scaling factor to fit the input Photograph into the default window size
     """
+    width = photo.get_image().shape[1]
+    height = photo.get_image().shape[0]
 
-    return min(defaults.max_window[0] / photo[0].shape[1],
-               defaults.max_window[1] / photo[0].shape[0])
+    return min(defaults.max_window[0] / width,
+               defaults.max_window[1] / height)
