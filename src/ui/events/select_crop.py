feea39dfb971e1582096ede9a82dc6e6b7659004
Allyson Robert <allyson.robert@aalto.fi>
2026-09-15 15:55:32 +0300

    running the point selection event is very similar for many situations, it basically only needs an image and a mode and returns the state and an action

--- src/ui/events/select_crop.py
+++ src/ui/events/select_crop.py
@@ -1,11 +1,8 @@
 from src.image.photograph import Photograph
 from src.colour_ops.convert_image_colour import convert_image_colour
-from src.ui.show_image import show_image
 import src.ui.events.point_selection as pt_selection
-import cv2 as cv
-
-from ui.events.keypress import wait_for_valid_keypress
 from utils.calc.get_corners_from_points import  get_corners_from_points
+from ui.events.run_point_selection import run_point_selection
 
 def select_crop(image: Photograph):
     """ Request user to select a crop region from the given image. The user is prompted to select a rectangular region
@@ -14,18 +11,7 @@ def select_crop(image: Photograph):
 
     converted_image = convert_image_colour(image)
 
-    callback_state = pt_selection.PointSelectionState()
-    context = pt_selection.PointSelectionCallbackContext(mode=pt_selection.PointSelectionMode.CROP,
-                                                                 image=converted_image)
-    keypress_action = None
-    while not (keypress_action or callback_state.is_complete()):
-        show_image(converted_image)
-        callback_callable = pt_selection.PointSelectionCallback(callback_state)
-        cv.setMouseCallback(context.mode.window_title,
-                            callback_callable,
-                            context)
-        keypress_action = wait_for_valid_keypress()
-    cv.destroyWindow(context.mode.window_title)
+    callback_state, keypress_action = run_point_selection(converted_image, pt_selection.PointSelectionMode.CROP)
 
     match keypress_action:
         case 'confirm':
