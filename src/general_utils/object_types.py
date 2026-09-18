ee0703d9cf1d54780a6c6d23543d138f2defc4c1
Allyson Robert <allyson.robert@aalto.fi>
2026-09-18 16:51:10 +0300

    CropPoints is now a defined dataclass

--- src/general_utils/object_types.py
+++ src/general_utils/object_types.py
@@ -1,4 +1,10 @@
+from dataclasses import dataclass
 from typing import Optional
 
 type Point = tuple[int, int]
 type OptionalPoint = Optional[Point]
+
+@dataclass
+class CropPoints:
+    top_left: Point
+    bottom_right: Point
