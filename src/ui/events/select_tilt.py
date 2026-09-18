1908113fbb515d83f7eb1c498c3b220960fb9957
Allyson Robert <allyson.robert@aalto.fi>
2026-09-18 15:02:31 +0300

    select_tilt should return the tilt

--- src/ui/events/select_tilt.py
+++ src/ui/events/select_tilt.py
@@ -1,12 +1,12 @@
 from src.image.photograph import Photograph
 from src.colour_ops.convert_image_colour import convert_image_colour
 import src.ui.events.point_selection as pt_selection
-
 from ui.events.run_point_selection import run_point_selection
+from src.general_utils.calc.get_angle_from_points import get_angle_from_points
 
 def select_tilt(image: Photograph):
     """ Request user to select a tilt line to rotate the given image. The user is prompted to select two points using
-    the mouse. The selected points are returned as a tuple of two points: the first and second points of the line."""
+    the mouse. The selected points are used to calculate and return the tilt angle."""
 
     converted_image = convert_image_colour(image)
 
@@ -15,7 +15,7 @@ def select_tilt(image: Photograph):
     match keypress_action:
         case 'confirm':
             callback_state.sort()
-            return callback_state.get_points()
+            return get_angle_from_points(*callback_state.get_points())
         case _:
             return None
 
